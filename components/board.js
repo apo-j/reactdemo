0c6fdeb6e44288640839b86d0e51e6ae3350cb1c
Jiyun Yang <jyang@talentsoft.com>
2016-12-21 18:32:09 +0100

    nothing

---
+++ components/board.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jyang on 21/12/2016.
+ */
+class Board extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+}
